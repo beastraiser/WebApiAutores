07dd592ba380194fb3a26e4cc7386b9813f06816
practicas.soporte <practicas.soporte@M2D123>
2024-04-03 16:37:56 +0200

    Agregar archivos de proyecto.

---
+++ WebApiAutores/Controllers/Entidades/Libro.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace WebApiAutores.Controllers.Entidades
+{
+    public class Libro
+    {
+        public int Id{ get; set; }
+        public string Titulo { get; set; }
+        public int AutorId { get; set; }
+        public Autor Autor { get; set; }
+    }
+}
