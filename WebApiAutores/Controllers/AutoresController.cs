6332ebf85e839c27e0eb25ce18b4a8181b3315ab
beastraiser <alex.bulc.96@gmail.com>
2024-04-04 09:29:22 +0200

    Commit 2

--- WebApiAutores/Controllers/AutoresController.cs
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -6,8 +6,8 @@ using WebApiAutores.Controllers.Entidades;
 namespace WebApiAutores.Controllers
 {
     [ApiController]
-    [Route("api/autores")]
-    public class AutoresController: ControllerBase
+    [Route("api/autores")] //Esta es la ruta a través de la cual se accede a este controlador. Se podría poner solo /autores pero por buenas prácticas se pone api/autores, para saber que se está accediendo a una api
+    public class AutoresController: ControllerBase //Esto es el controlador => clase que define la tabla y contiene los métodos para gestionar esa tabla (endpoints)
     {
 
         private readonly ApplicationDbContext context;
@@ -17,13 +17,19 @@ namespace WebApiAutores.Controllers
             this.context = context;
         }
 
-        [HttpGet]
-        public async Task<ActionResult<List<Autor>>> Get()
+        [HttpGet] //Si se ejecuta una petición GET hacia este controlador, se ejecutará el código que hay dentro de este bloque
+        public async Task<ActionResult<List<Autor>>> Get() //Este endpoint devuelve la lista con todos los datos de la tabla Autor. La ruta es "api/autores"
         {
             return await context.Autores.Include(x => x.Libros).ToListAsync();
         }
 
-        [HttpPost]
+        [HttpGet("primero")] //Este es otro endpoint GET cuya ruta va a ser "api/autores/primero". Si no se indica ninguna cadena dará error ya que habrá dos endpoints GET para la misma ruta
+        public async Task<ActionResult<Autor>> PrimerAutor() //Este endpoint devuelve el primer autor de la tabla Autor o un null si no hay ningún valor
+        {
+            return await context.Autores.FirstOrDefaultAsync();
+        }
+
+        [HttpPost] //Si se ejecuta una petición POST hacia este controlador, se ejecutará el código que hay dentro de este bloque
         public async Task<ActionResult> Post(Autor autor)
         {
             context.Add(autor);
